f7b693e1556a43508ff6d915df6c9d13c28ff548
Heorhii Vapnychnyi <Heorhii.Vapnychnyi@cs.khpi.edu.ua>
2026-02-25 07:23:41 +0100

    Initial commit: environment setup and reverse logic

---
+++ reverse_letters.py
@@ -0,0 +1,30 @@
+def revers_word(word: str) -> str:
+    """returns a reversed version of given word"""
+    letters_only: list[str] = [char for char in word if char.isalpha()]
+
+    result: list[str] = []
+    for char in word:
+        if char.isalpha():
+            result.append(letters_only.pop())
+        else:
+            result.append(char)
+
+    return "".join(result)
+
+def revers_text(text: str) -> str:
+    """returns a reversed version words in given text"""
+    words: list[str] = text.split(" ")
+
+    reversed_words: list[str] = [revers_word(word) for word in words]
+
+    return " ".join(reversed_words)
+
+if __name__=="__main__":
+    cases = [
+        ("abcd efgh", "dcba hgfe"),
+        ("a1bcd efg!h", "d1cba hgf!e"),
+        ("", ""),
+    ]
+
+    for text, reversed_text in cases:
+        assert revers_text(text) == reversed_text
